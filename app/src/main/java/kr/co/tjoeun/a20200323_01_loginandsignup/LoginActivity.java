67c73504370f05b5ade7c67531eb807a7618ef3b
hjin302 <enro302@naver.com>
2020-03-23 16:50:22 +0900

    메인액티비티 -> 로그인액티비티로 변경
     - 클래스 이름 변경
     - xml 이름 변경
     - 데이터바인딩 클래스 재설정

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/LoginActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/LoginActivity.java
@@ -1,6 +1,5 @@
 package kr.co.tjoeun.a20200323_01_loginandsignup;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
@@ -13,19 +12,20 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityMainBinding;
+import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityLoginBinding;
+import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivityLoginBinding;
 import kr.co.tjoeun.a20200323_01_loginandsignup.datas.User;
 import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ContextUtil;
 import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ServerUtil;
 
-public class MainActivity extends BaseActivity {
+public class LoginActivity extends BaseActivity {
 
-    ActivityMainBinding binding = null;
+    ActivityLoginBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_login);
 
         setupEvents();
         setValues();
