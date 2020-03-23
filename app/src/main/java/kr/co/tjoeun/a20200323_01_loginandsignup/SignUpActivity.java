45333f64fc67802671f82828c53c8d944aa67696
cho881020 <kj_cho@nepp.kr>
2020-03-23 15:49:49 +0900

    회원가입 서버 전달까지.  => DB에서 가입 성공 확인  => UI 반영은 안한 상태. (로그로만 확인)

--- app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/SignUpActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/SignUpActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONObject;
 
 import kr.co.tjoeun.a20200323_01_loginandsignup.databinding.ActivitySignUpBinding;
+import kr.co.tjoeun.a20200323_01_loginandsignup.utils.ServerUtil;
 
 public class SignUpActivity extends BaseActivity {
 
@@ -21,6 +26,32 @@ public class SignUpActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                ID / 비번 / 이름 /폰번을 따와서 => 서버로 전달
+//                회원가입 API에 전달
+
+                String inputId = binding.idEdt.getText().toString();
+                String inputPw = binding.pwEdt.getText().toString();
+                String inputName = binding.nameEdt.getText().toString();
+                String inputPhone = binding.phoneEdt.getText().toString();
+
+                ServerUtil.putRequestSignUp(mContext, inputId, inputPw, inputName, inputPhone, new ServerUtil.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+
+                        Log.d("회원가입", json.toString());
+
+                    }
+                });
+
+
+
+            }
+        });
 
     }
 
